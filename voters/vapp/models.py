c7bf821d9e8414175e8ceaed62b2d2745b953bdc
Yuvalneti41 <yuvalpro6@gmail.com>
2025-07-17 11:41:30 +0300

    Update models.py

--- voters/vapp/models.py
+++ voters/vapp/models.py
@@ -1,3 +1,54 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
+
+class Politikai(models.Model):
+    profile_picture = models.ImageField(upload_to='govener_profile_pics/')
+    name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    age = models.IntegerField()
+    country_of_origin = models.CharField(max_length=50)
+    biography = models.TextField(max_length=1000)
+    
+    political_party = models.CharField(max_length=100)
+    political_past = models.TextField(max_length=1000)
+    political_opinion = models.TextField(max_length=1000)
+    
+    millitery_rank = models.CharField(max_length=100)
+    millitery_past = models.TextField(max_length=500)
+
+    education = models.TextField(max_length=500)
+    way_of_living = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"politikai: {self.name + ' ' + self.last_name}"
+    
+
+
+
+class Goverment(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    party_leader = models.ForeignKey(Politikai, on_delete=models.CASCADE)
+    party_ceo_or_secretary_general = models.ForeignKey(Politikai, on_delete=models.CASCADE)
+    party_spokesperson = models.ForeignKey(Politikai, on_delete=models.CASCADE)
+    treasurer_or_finance_manager  = models.ForeignKey(Politikai, on_delete=models.CASCADE)
+    strategic_advisor_or_campaign_manager = models.ForeignKey(Politikai, on_delete=models.CASCADE)
+    regional_activists_or_branch_leaders  = models.ForeignKey(Politikai, on_delete=models.CASCADE)
+    party_member_one = models.ForeignKey(Politikai, on_delete=models.CASCADE)
+    party_member_two = models.ForeignKey(Politikai, on_delete=models.CASCADE)
+    party_member_three = models.ForeignKey(Politikai, on_delete=models.CASCADE)
+    party_member_four = models.ForeignKey(Politikai, on_delete=models.CASCADE)
+    party_member_five = models.ForeignKey(Politikai, on_delete=models.CASCADE)
+    party_member_six = models.ForeignKey(Politikai, on_delete=models.CASCADE)
+    party_member_seven = models.ForeignKey(Politikai, on_delete=models.CASCADE)
+    party_member_eight = models.ForeignKey(Politikai, on_delete=models.CASCADE)
+    party_member_nine = models.ForeignKey(Politikai, on_delete=models.CASCADE)
+    party_member_ten = models.ForeignKey(Politikai, on_delete=models.CASCADE)
+    party_member_eleven = models.ForeignKey(Politikai, on_delete=models.CASCADE)
+    party_member_twelve = models.ForeignKey(Politikai, on_delete=models.CASCADE)
+    party_member_thirteen = models.ForeignKey(Politikai, on_delete=models.CASCADE)
+    party_member_fourteen = models.ForeignKey(Politikai, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"Custom Goverment by: {self.owner_profile.user.first_name + ' ' + self.owner_profile.user.last_name}"
